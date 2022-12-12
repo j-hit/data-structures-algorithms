49005219e3b27243b58b3c483f3c9b43e0415321
James Bampoe <james.bampoe@git.com>
2022-12-12 23:44:37 +0100

    refactor: add comments

--- Arrays/Leetcode/1-Maximum-Subarray.ts
+++ Arrays/Leetcode/1-Maximum-Subarray.ts
@@ -1,12 +1,27 @@
-// https://leetcode.com/problems/maximum-subarray/
+/*
+  https://leetcode.com/problems/maximum-subarray/
+
+  Given an integer array nums, find the subarray which has the largest sum and return its sum.
+
+  Example 1:
+
+  Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+  Output: 6
+  Explanation: [4,-1,2,1] has the largest sum = 6.
+  
+  Example 2:
+
+  Input: nums = [1]
+  Output: 1
+  
+  Example 3:
+
+  Input: nums = [5,4,-1,7,8]
+  Output: 23
+
+*/
 
 function maxSubArray(nums: number[]): number {
-  // [-1,1,2,1]
-  // Output
-  // 3
-  // Expected
-  // 4
-
   if (nums.length === 1) {
     return nums[0];
   }
