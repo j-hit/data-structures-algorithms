9c998bcf098de40c258e2718d54a73d3f5b92daa
James Bampoe <james.bampoe@git.com>
2023-01-23 22:05:24 +0100

    feat: add reverse string solution

---
+++ Recursion/Leetcode/1-Reverse-String.ts
@@ -0,0 +1,67 @@
+/**
+ * https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1440/
+ *
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ *
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ *
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ *
+ * Input: s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 10^5
+ * s[i] is a printable ascii character.
+ */
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  recursivelyReverse(s, 0, s.length - 1);
+}
+
+const recursivelyReverse = (
+  s: string[],
+  startIndex: number,
+  endIndex: number
+): void => {
+  if (startIndex === endIndex) {
+    return;
+  }
+
+  if (startIndex > endIndex) {
+    return;
+  }
+
+  if (endIndex < startIndex) {
+    return;
+  }
+
+  let first = s[startIndex];
+  let last = s[endIndex];
+  s[startIndex] = last;
+  s[endIndex] = first;
+
+  recursivelyReverse(s, startIndex + 1, endIndex - 1);
+};
+
+let array = ['h', 'e', 'l', 'l', 'o'];
+reverseString(array);
+console.log(array);
+
+/**
+ *
+ * a = a
+ * ab = ba
+ *
+ * abbc = cbba
+ *
+ * abcde = edcba
+ *
+ */
